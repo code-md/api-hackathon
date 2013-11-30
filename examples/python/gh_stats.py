45aaf25655cc7fb55ab49b2d59bdecafe069b411
Ivan Zarea <zarea.ion@gmail.com>
2013-11-30 13:12:00 +0200

    Switched to spaces instead of tabs

--- examples/python/gh_stats.py
+++ examples/python/gh_stats.py
@@ -9,12 +9,12 @@ BASE_URL = 'https://api.github.com/'
 PATH = 'users/{}/repos'
 
 def getUsersStats(username):
-  request = urllib2.urlopen('{}{}'.format(BASE_URL, PATH.format(username)))
-  data = request.read()
-  return json.loads(data)
+    request = urllib2.urlopen('{}{}'.format(BASE_URL, PATH.format(username)))
+    data = request.read()
+    return json.loads(data)
 
 def showUsersStats(username):
-	repos = getUsersStats(username)
+    repos = getUsersStats(username)
 
-	for rep in repos:
-		print 'Repo: {} Stars: {} Forks: {}'.format(rep['name'], rep['stargazers_count'], rep['forks_count'])
+    for rep in repos:
+        print 'Repo: {} Stars: {} Forks: {}'.format(rep['name'], rep['stargazers_count'], rep['forks_count'])
